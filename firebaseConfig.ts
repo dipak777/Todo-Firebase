acefbb15de99517e613628541d450d3b17f6bdab
Dipak Shrimali <dipakdk777@gmail.com>
2024-05-08 10:50:48 +0530

    Todo List checkbox and delete operation complete

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+// import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCIJG3tsxeR5erUhjtpRAqSkPDeHdIEzxA",
+  authDomain: "fireapp-3ab58.firebaseapp.com",
+  projectId: "fireapp-3ab58",
+  storageBucket: "fireapp-3ab58.appspot.com",
+  messagingSenderId: "405897882766",
+  appId: "1:405897882766:web:ca6888716f69c95482addf",
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+// export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
